a0fdc52f7751a70bb806fbc1c6f64aa8fe987f2c
Arvin Kenn delos Santos <adsantos.417@gmail.com>
2023-04-16 15:41:42 +0800

    Create NoteList component

---
+++ src/components/NotesList/index.tsx
@@ -0,0 +1,43 @@
+import { IconButton, Paper } from "@mui/material";
+import { FC, ReactNode } from "react";
+import Grid from "@mui/material/Grid";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import useNotesStore from "../../store/useNotesStore";
+
+const NotesList: FC = () => {
+  const { notes } = useNotesStore();
+  return (
+    <Grid
+      container
+      spacing={4}
+      sx={{
+        marginTop: "1rem",
+      }}
+    >
+      {notes.map((n) => (
+        <Grid key={n.id} item lg={4} md={3}>
+          <Paper
+            elevation={4}
+            sx={{
+              padding: "1rem",
+              textAlign: "left",
+            }}
+          >
+            <p>{n.text}</p>
+            <div>
+              <IconButton color="warning" component="label">
+                <EditIcon />
+              </IconButton>
+              <IconButton color="error" component="label">
+                <DeleteIcon />
+              </IconButton>
+            </div>
+          </Paper>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default NotesList;
